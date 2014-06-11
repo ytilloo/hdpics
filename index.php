823888e0e65843ad4cd77740ef31848d8310de18
ytilloo <yogeshtilloo@ytmac.local>
2014-06-11 10:38:20 +0530

    index php added

---
+++ index.php
@@ -0,0 +1,5 @@
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+<title>HDpics mobile</title>
+<link rel="apple-touch-icon" href="icons/icon_hdpics_ios.png"/>
+
+<?php include("m_hdpics.php");?>
